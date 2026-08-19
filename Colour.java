56ea628950e8bf7ea9a3a4a19bb8cee874cfdd85
Saieeweb <saieejagtap70@gmail.com>
2026-08-19 12:49:01 +0530

    message pass

---
+++ Colour.java
@@ -0,0 +1,10 @@
+public class Colour{
+public static void main (String args[]){
+System.out.println ("blue");
+}
+}
+class Notebook{ 
+public static void main(String args[]){
+System.out.println ("classmate");
+}
+}
