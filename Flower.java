d9bd3a6f4aff0fa236377afb1164cfd66c0f9ab3
Saieeweb <saieejagtap70@gmail.com>
2026-08-19 16:28:05 +0530

    nature

---
+++ Flower.java
@@ -0,0 +1,10 @@
+public class Flower{
+public static void main(String args[]){
+System.out.println ("Sunflower");
+}
+}
+class Fruit{
+public static void main(String args[]){
+System.out.println ("Watermelon");
+}
+}
