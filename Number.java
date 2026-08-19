9c6a2624cc8099ac03f1b3392c802193e4e64bf7
Saieeweb <saieejagtap70@gmail.com>
2026-08-19 21:55:43 +0530

    study

---
+++ Number.java
@@ -0,0 +1,10 @@
+public class Number{
+public static void main (String args[]){
+System.out.println ("One");
+}
+}
+class Subject{
+public static void main (String args[]){
+System.out.println ("Math");
+}
+}
