e9792e6e789c3a48132dcbacf4ee3fa6e99fc1bf
Saieeweb <saieejagtap70@gmail.com>
2026-08-19 16:24:14 +0530

    great voice quality

---
+++ Headphone.java
@@ -0,0 +1,10 @@
+public class Headphone{
+public static void main (String args[]){
+System.out.println ("Jbl");
+}
+}
+class Earbuds{
+public static void main(String args[]){
+System.out.println ("Boat");
+}
+}
