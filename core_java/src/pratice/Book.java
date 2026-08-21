a7cc0278d21fa60cfb556072f8d3b581b7052d57
Saieeweb <saieejagtap70@gmail.com>
2026-08-21 12:36:20 +0530

    Second

---
+++ core_java/src/pratice/Book.java
@@ -0,0 +1,13 @@
+package pratice;
+
+public class Book {
+int a;
+int b;
+int res;
+public static void main(String args[]) {
+	int a=78;
+	int b=99;
+	int res=a+b;
+	System.out.println("res"+ res);
+}
+}
