575022fd717046d9699ba3099f35a23306556705
Saieeweb <saieejagtap70@gmail.com>
2026-08-21 12:45:45 +0530

    third

---
+++ core_java/src/pratice/Multipation.java
@@ -0,0 +1,17 @@
+package pratice;
+
+public class Multipation {
+	int a;
+	int b;
+	int res;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=35;
+	    int b=45;
+	    int res=a*b;
+		System.out.println("res="  +res);
+
+	}
+
+}
