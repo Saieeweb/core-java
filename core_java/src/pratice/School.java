87efd8810dda05a4db0983816c9ea6897c69085b
Saieeweb <saieejagtap70@gmail.com>
2026-08-24 22:35:03 +0530

    Student

---
+++ core_java/src/pratice/School.java
@@ -0,0 +1,29 @@
+package pratice;
+
+public class School {
+	int teachers;
+	byte age;
+	float marks;
+	short birth;
+	char gander;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Static public void main (Sting[] args) {
+			teachers=34;
+			age=23;
+			marks=78.f;
+			birth=2004;
+			gander='m';
+			System.out.print("teachers");
+
+			System.out.print("age");
+
+			System.out.print("marks");
+
+			System.out.print("birth");
+
+			System.out.print("gander");
+
+	}
+
+}
