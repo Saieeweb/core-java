06fac3545db55cd2f2c2d466687dfc14ec624da7
Saieeweb <saieejagtap70@gmail.com>
2026-08-24 12:38:49 +0530

    Student

---
+++ core_java/src/pratice/Primitivedata.java
@@ -0,0 +1,28 @@
+package pratice;
+
+public class Primitivedata {
+		
+	int Marks=32;
+	byte Age=22;
+	float Perctange=87.8765f;
+	double Salary=70000;
+	char Class='B';
+	short id=34;
+	
+
+	Public static void main (String[]args) {
+	
+		
+	
+	System.out.println("Marks = "+ Marks);
+	System.out.println("Age = " + Age);
+	System.out.println("Perctange = " + perctange);
+	System.out. println("Salary = " + Salary);
+	System.out.println("Class = " + Class);
+	System.out.println("id = " + id);
+		
+		
+
+	}
+
+}
---
+++ core_java/src/pratice/Student.java
@@ -0,0 +1,32 @@
+package pratice;
+
+public class Student {
+static byte age;
+static 	short birth;
+static	int sub;
+static	float marks;
+static	double per;
+static	char gender;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		age =22;
+		birth=2004;
+		sub =24;
+		marks=127.5434f;
+		per=90.7654674545;
+		gender='f';
+		System.out.println(age);
+		System.out.println(birth);
+		System.out.println(sub);
+		System.out.println(marks);
+		System.out.println(per);
+		System.out.println(gender);
+		
+		
+		
+
+	}
+
+}
