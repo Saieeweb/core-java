f0228ec6a63bbc80ea538fba40cd5ec22121e1b6
Saieeweb <saieejagtap70@gmail.com>
2026-08-24 11:25:21 +0530

    Marks

---
+++ core_java/src/pratice/Marks.java
@@ -0,0 +1,18 @@
+package pratice;
+
+public class Marks {
+	int Math;
+	int Sci;
+	int Eng;
+	int res;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Math=76;
+		int Sci=88;
+		int Eng=78;
+		int res=Math+Sci+Eng;
+		System.out.println("res" +res);
+	}
+
+}
