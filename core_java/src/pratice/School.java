4bbcd2b1f7523e1efdaa12de5e9d86a0db6e35ba
Saieeweb <saieejagtap70@gmail.com>
2026-08-25 23:53:18 +0530

    unary

--- core_java/src/pratice/School.java
+++ core_java/src/pratice/School.java
@@ -5,24 +5,24 @@ public class School {
 	byte age;
 	float marks;
 	short birth;
-	char gander;
+	char gander; 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Static public void main (Sting[] args) {
-			teachers=34;
-			age=23;
-			marks=78.f;
-			birth=2004;
-			gander='m';
-			System.out.print("teachers");
+			teachers =34;
+			age =23;
+			marks =78.f;
+			birth =2004;
+			gander ='m';
+			System.out.println("teachers");
 
-			System.out.print("age");
+			System.out.println("age");
 
-			System.out.print("marks");
+			System.out.println("marks");
 
-			System.out.print("birth");
+			System.out.println("birth");
 
-			System.out.print("gander");
+			System.out.println("gander");
 
 	}
 
---
+++ core_java/src/pratice/unary.java
@@ -0,0 +1,17 @@
+package pratice;
+
+public class unary {
+	int a;
+	
+
+	public static void main(String[] args) {
+		int a=5;
+		System.out.println(++a + --a + ++a + a--);
+		System.out.println(--a + ++a + a-- + a++);
+		System.out.println(++a + --a + --a + a++);
+		System.out.println(++a + ++a + --a + a++);
+		
+	}
+}
+
+	
