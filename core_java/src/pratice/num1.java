94020f339ff9ee6baf35a90b2e3e24f60029fccd
Saieeweb <saieejagtap70@gmail.com>
2026-08-25 23:59:52 +0530

    num1

---
+++ core_java/src/pratice/num1.java
@@ -0,0 +1,16 @@
+package pratice;
+
+public class num1 {
+	int num;
+
+	public static void main(String[] args) {
+		int num=15;
+		System.out.println(++num + --num + num++ + --num);
+		System.out.println(--num + num-- + num++ + --num);
+		System.out.println(++num + num++ + num-- + num--);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
