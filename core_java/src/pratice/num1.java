bc97febb4b1ef30d9f2ffa3cb0f919859193aa57
Saieeweb <saieejagtap70@gmail.com>
2026-08-26 11:57:09 +0530

    Num2

--- core_java/src/pratice/num1.java
+++ core_java/src/pratice/num1.java
@@ -4,11 +4,12 @@ public class num1 {
 	int num;
 
 	public static void main(String[] args) {
-		int num=15;
+		int num=20;
 		System.out.println(++num + --num + num++ + --num);
 		System.out.println(--num + num-- + num++ + --num);
 		System.out.println(++num + num++ + num-- + num--);
-		
+		System.out.println(++num + num-- + --num + num++);
+		System.out.println(--num + num++ + --num + --num);
 		// TODO Auto-generated method stub
 
 	}
