3de09ce79735866d2bbdfcea710c2e606234f919
Saieeweb <saieejagtap70@gmail.com>
2026-08-27 12:19:35 +0530

    odd

---
+++ core_java/src/pratice/oddnumber.java
@@ -0,0 +1,21 @@
+package pratice;
+
+public class oddnumber {
+
+	public static void main(String[] args) {
+		int num=765;
+		
+		if (num%2 !=0) {
+			System.out.println("odd number"); 
+		}
+			
+			else 
+			{ 
+				System.out.println("even number");
+			}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
