ea94ae19c469f26def2875ca3829fa710227b1e8
Saieeweb <saieejagtap70@gmail.com>
2026-08-27 12:46:42 +0530

    teen

---
+++ core_java/src/pratice/AgeExample.java
@@ -0,0 +1,19 @@
+package pratice;
+
+public class AgeExample {
+
+	public static void main(String[] args) { 
+		int age=17;
+		
+		if (age<=14) {
+			System.out.println("your teen");
+		}
+		else {
+			System.out.println("your not teen");
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
