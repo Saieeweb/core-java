c7d757dd38a65605b8ccf6ea86fe96f62351654f
Saieeweb <saieejagtap70@gmail.com>
2026-08-27 12:01:08 +0530

    salary

---
+++ core_java/src/pratice/conditional1.java
@@ -0,0 +1,19 @@
+package pratice;
+
+public class conditional1 {
+
+	public static void main(String[] args) {
+		double salary=80000.00;
+		
+		if (salary>=100000)
+		{
+			System.out.println("great");
+		}
+		else 
+		{
+			System.out.println("its ok");
+		}
+
+	}
+
+}
---
+++ core_java/src/pratice/unary4.java
@@ -0,0 +1,10 @@
+package pratice;
+
+public class unary4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
