11bfbcf76b2c5f4c421b43687160dc94174de7bc
Saieeweb <saieejagtap70@gmail.com>
2026-08-29 16:39:44 +0530

    And

---
+++ core_java/src/pratice/ANDlog.java
@@ -0,0 +1,13 @@
+package pratice;
+
+public class ANDlog {
+	
+
+	public static void main(String[] args) {
+    int age=18;
+    int weight=89;
+    System.out.println(age >=23 && weight <=90 );
+    System.out.println(age !=18 && weight ==89);
+	}
+
+}
--- core_java/src/pratice/Addition.java
+++ core_java/src/pratice/Addition.java
@@ -1,9 +1,15 @@
 package pratice;
 
 public class Addition {
+int a;
+int b;
+int res;
 
 	public static void main(String[] args) {
-		Systm.out.println("37+44");
+		int a=56;
+		int b=76;
+		int res=a+b;
+		System.out.println("res="res);
 		// TODO Auto-generated method stub
 
 	}
