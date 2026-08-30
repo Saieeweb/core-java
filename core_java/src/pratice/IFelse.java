dedd520b3563d69e60e103718bdbff032c7c8c36
Saieeweb <saieejagtap70@gmail.com>
2026-08-30 21:44:15 +0530

    minor

---
+++ core_java/src/pratice/IFelse.java
@@ -0,0 +1,22 @@
+package pratice;
+
+public class IFelse {
+
+	public static void main(String[] args) {
+		int age=18;
+		
+		if
+		(age <=14)
+		{
+			System.out.println("Adult");
+			
+		}
+		
+		else 
+			
+		{ 
+			System.out.println("Minor");
+		}
+	}
+
+}
