904582dbb0f184fb250f48f678177bd3f3c37095
Saieeweb <saieejagtap70@gmail.com>
2026-08-30 21:35:17 +0530

    School caption

---
+++ core_java/src/pratice/ORlog1.java
@@ -0,0 +1,29 @@
+package pratice;
+
+public class ORlog1 {
+
+	public static void main(String[] args) {
+		int  player=4;
+		char winer='T';
+		short price=3000;
+		boolean goodcaption=true;
+		
+		if 
+			
+		(player <= 4 || winer == 'T' || price >= 3000 || goodcaption== true)
+		{
+			System.out.println(" caption of scchool team");
+			
+			
+		}
+		else 
+		{
+			System.out.println(" not caption of school team");
+		}
+		
+		
+		
+
+	}
+
+}
