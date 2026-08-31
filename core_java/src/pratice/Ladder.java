af7727540873c628548449ad830dd25a69597ee3
Saieeweb <saieejagtap70@gmail.com>
2026-08-31 11:22:03 +0530

    pair

---
+++ core_java/src/pratice/Ladder.java
@@ -0,0 +1,30 @@
+package pratice;
+
+public class Ladder {
+
+	public static void main(String[] args) 
+	{
+
+		int marks=80;
+		
+		if (marks >= 35 && marks <= 50)
+		{
+			System.out.println("C garde");
+		}
+		else if (marks >= 51 && marks <=60)
+		{
+			System.out.println("B grade");
+		}
+		else if (marks >=61 && marks <=74) 
+		{
+			System.out.println("A");
+			
+		}
+		else 
+		{
+			System.out.println("fail");
+		}
+	}
+	
+
+}
