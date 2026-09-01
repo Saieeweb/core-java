57f78a811af82a60dcadc6ba5f9d8e6426b35fce
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 13:42:38 +0530

    odd2

---
+++ core_java/src/pratice/LoopOdd.java
@@ -0,0 +1,19 @@
+package pratice;
+
+public class LoopOdd {
+
+	public static void main(String[] args) {
+int num=51;
+
+while (num<=100)
+{
+	if ( num%3==0)
+	{
+		System.out.println(num);
+	}
+	num++;
+}
+
+	}
+
+}
