739e19fdc531fd909c742c913189cb733691afee
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 12:10:48 +0530

    num2

---
+++ core_java/src/pratice/Loop2.java
@@ -0,0 +1,21 @@
+package pratice;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+int num=500;
+int counter=0;
+while
+	(num<=2000)
+{
+	if(num%2==0) {
+     counter++;
+     
+     System.out.println(counter);
+	}
+	num++;
+}
+System.out.println(counter);
+	}
+
+}
