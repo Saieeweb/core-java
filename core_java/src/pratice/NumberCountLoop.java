0ff9568ad29fed84a0b197eedbd227085a11eadf
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 22:26:55 +0530

    numbers

---
+++ core_java/src/pratice/NumberCountLoop.java
@@ -0,0 +1,14 @@
+package pratice;
+
+public class NumberCountLoop {
+
+	public static void main(String[] args) {
+int num=1;
+    while (num<=100)
+    {
+   System.out.println(num);
+   num++;
+    }
+	}
+
+}
