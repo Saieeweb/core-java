60f07260b5dfcb25873702a9c46a6e87e9e8ae0c
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 22:19:55 +0530

    sum

---
+++ core_java/src/pratice/Loop6.java
@@ -0,0 +1,20 @@
+package pratice;
+
+public class Loop6 {
+
+	public static void main(String[] args) {
+int num=1;
+int sum=0;
+    while (num<=100)
+    {
+    	if(num%2==0){
+    		sum+=num;
+    	}
+    		num++;
+    }
+    System.out.println(sum);
+    
+    }
+	}
+
+
