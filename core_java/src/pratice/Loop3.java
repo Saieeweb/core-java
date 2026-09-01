148756315e922d98be40138f82793b43d0ebaa38
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 12:26:30 +0530

    num3

---
+++ core_java/src/pratice/Loop3.java
@@ -0,0 +1,24 @@
+package pratice;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+int num=500;
+int counter=0;
+
+while 
+	(num<=1000)
+{
+	if 
+	(num%2==0 && num%6==0 && num%8==0 )
+	{
+		counter++;
+		System.out.println(num);
+	}
+	num++;
+}
+System.out.println("the count value");
+System.out.println(counter);
+	}
+
+}
