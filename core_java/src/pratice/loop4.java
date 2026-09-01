5275c483f90faf19cb091e0020f05d60d1f737f0
Saieeweb <saieejagtap70@gmail.com>
2026-09-01 13:20:50 +0530

    odd number

---
+++ core_java/src/pratice/loop4.java
@@ -0,0 +1,28 @@
+package pratice;
+
+public class loop4 {
+
+	public static void main(String[] args) {
+int num=1;//intialiazation
+
+while (num<=15)//condition
+		
+	{
+	if(num%2!=0)
+	{
+	
+		System.out.println(num);
+	}
+	num++; //increment
+		
+	}
+
+	}
+
+
+
+
+
+	}
+
+
