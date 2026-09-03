5596ca245fdc247864d420b6a7c309b672aac679
Saieeweb <saieejagtap70@gmail.com>
2026-09-03 12:28:13 +0530

    Break

---
+++ core_java/src/pratice/FORLOOP3.java
@@ -0,0 +1,16 @@
+package pratice;
+
+public class FORLOOP3 {
+
+	public static void main(String[] args) {
+for(int i=1;i<=10;i++)
+{
+	if(i==5)
+	{
+		break;
+	}
+	System.out.println(i);
+}
+	}
+
+}
