1c6a3a265119f895075e9deda305f508c82f996a
Saieeweb <saieejagtap70@gmail.com>
2026-09-03 12:32:50 +0530

    CONTINUE

--- core_java/src/pratice/FORLOOP3.java
+++ core_java/src/pratice/FORLOOP3.java
@@ -3,11 +3,11 @@ package pratice;
 public class FORLOOP3 {
 
 	public static void main(String[] args) {
-for(int i=1;i<=10;i++)
+for(int i=1;i<=20;i++)
 {
-	if(i==5)
+	if(i==20)
 	{
-		break;
+		continue;
 	}
 	System.out.println(i);
 }
