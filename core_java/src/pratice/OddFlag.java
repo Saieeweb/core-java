308e6683d015fb9cc8fd967970ad11e256064492
Saieeweb <saieejagtap70@gmail.com>
2026-09-04 12:59:27 +0530

    OddNumber

---
+++ core_java/src/pratice/OddFlag.java
@@ -0,0 +1,22 @@
+package pratice;
+
+public class OddFlag {
+
+	public static void main(String[] args) {
+int number=9;
+boolean flag=false;
+if (number%2==0)
+{
+	flag=true;
+}
+if (flag)
+{
+	System.out.println("Number is even");
+} 
+else
+{
+	System.out.println("Number is odd");
+}
+	}
+
+}
