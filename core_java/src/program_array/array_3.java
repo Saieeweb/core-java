7372281835e469625a3a9df89ec519fe7d2de648
Saieeweb <saieejagtap70@gmail.com>
2026-09-22 13:56:21 +0530

    array3

---
+++ core_java/src/program_array/array_3.java
@@ -0,0 +1,13 @@
+package program_array;
+
+public class array_3 {
+
+	public static void main(String[] args) {
+int[]number= { 10, 20, 30, 40, 50};
+for ( int i=0; i<number.length; i++)
+{
+	System.out.println(number[i]);
+}
+	}
+
+}
