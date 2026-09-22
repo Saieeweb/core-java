2b85c0dca4300bd88252da9855b8253477f27584
Saieeweb <saieejagtap70@gmail.com>
2026-09-22 21:07:45 +0530

    array5

---
+++ core_java/src/program_array/array_5.java
@@ -0,0 +1,16 @@
+package program_array;
+
+public class array_5 {
+
+	public static void main(String[] args) {
+int [] number= {10, 20, 30, 40, 50};
+int sum=0;
+for (int i=0; i<number.length; i++) {
+	sum=sum+number[i];
+	
+}
+System.out.println("sum="+sum);
+
+	}
+
+}
