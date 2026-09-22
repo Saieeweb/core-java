a6374245306aeb8a02188e0c2b6c83c44dbd6196
Saieeweb <saieejagtap70@gmail.com>
2026-09-22 20:55:19 +0530

    array4

---
+++ core_java/src/program_array/array_4.java
@@ -0,0 +1,13 @@
+package program_array;
+
+public class array_4 {
+
+	public static void main(String[] args) {
+String[]name= {"saiee", "tanvi", "nandini", "sakshi",};
+for (int i=0; i<name.length; i++)
+{
+	System.out.println (name[i]);
+}
+	}
+
+}
