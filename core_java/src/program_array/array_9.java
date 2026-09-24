ddb0551307069977e30671973794dd71caa47cae
Saieeweb <saieejagtap70@gmail.com>
2026-09-24 12:29:58 +0530

    array9

---
+++ core_java/src/program_array/array_9.java
@@ -0,0 +1,18 @@
+package program_array;
+
+public class array_9 {
+
+	public static void main(String[] args) {
+		int [] number= {10,20,30,40,50};
+		int sum=0;
+		for (int i=0;i<=number.length; i++)
+		{
+			sum=sum+number[i];
+		}
+		System.out.println("sum="+sum);
+		}
+		}
+
+	
+
+
