b8b0cb70e4916e92649f6d2cb3deb1abc910e8e1
Saieeweb <saieejagtap70@gmail.com>
2026-09-24 13:42:58 +0530

    array22d

--- core_java/src/program_array/array2_2d.java
+++ core_java/src/program_array/array2_2d.java
@@ -8,15 +8,21 @@ public class array2_2d {
 				{879,765,345},
 				{123,543,768}
 		};
+		int sum=0;
 		for (int i=0; i<3; i++)
 		{
 			for (int j=0; j<3; j++)
 			{
 				if (numbers[i][j]%2==0) {
 					System.out.println(numbers[i][j]);
+					sum=sum+numbers[i][j];
+					
 				}
+				
+				//System.out.println("sum is: " + sum);
 			}
 		}
+		System.out.println("sum is: " + sum);
 
 	}
 
