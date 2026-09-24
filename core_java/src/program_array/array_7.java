0b1b5f0a931447ac06cc86818e780277bdade648
Saieeweb <saieejagtap70@gmail.com>
2026-09-24 10:56:55 +0530

    array8

--- core_java/src/program_array/array_7.java
+++ core_java/src/program_array/array_7.java
@@ -4,14 +4,21 @@ public class array_7 {
 
 	public static void main(String[] args) {
 		int[] numbers = {10, 15, 22, 31, 40, 55};
+		int sum=0;
+		int counter=0;
 
         for(int i = 0; i < numbers.length; i++) {
 
             if(numbers[i] % 2 == 0) {
                 System.out.println(numbers[i]);
+                counter++;
+                sum=sum+numbers[i];
+                
             }
         }
-    }
+        System.out.println("Total even number="+ counter);
+        System.out.println("Total even number sum="+sum);
+        }
 }
 
 
---
+++ core_java/src/program_array/array_8.java
@@ -0,0 +1,14 @@
+package program_array;
+
+public class array_8 {
+
+	public static void main(String[] args) {
+		int i;
+int [] number = {10,20, 30, 40, 50 };
+for (i=number.length-1; i>=0; i--)
+{
+	System.out.println(number[i]);
+}
+	}
+
+}
