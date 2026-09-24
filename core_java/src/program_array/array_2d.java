de2f6b85619a1daf6f6cf2542b70c776ff9905a0
Saieeweb <saieejagtap70@gmail.com>
2026-09-24 13:17:32 +0530

    array2d

---
+++ core_java/src/program_array/array_2d.java
@@ -0,0 +1,21 @@
+package program_array;
+
+public class array_2d {
+
+	public static void main(String[] args) {
+int [][]number= {
+		{10,20,30},
+		{40,50,60},
+		{40,50,60}};
+for (int i=0; i<3; i++)
+{
+	for (int j=0; j<3; j++)
+	{
+		System.out.print(number[i][j]+ " ");
+	}
+	System.out.println();
+	}
+}
+	}
+
+
