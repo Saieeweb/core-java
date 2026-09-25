36e088fe1bd29cdebb4573e3c35ca36541d69952
Saieeweb <saieejagtap70@gmail.com>
2026-09-25 22:16:29 +0530

    array2d

---
+++ core_java/src/program_array/array7_2d.java
@@ -0,0 +1,18 @@
+package program_array;
+
+public class array7_2d {
+
+	public static void main(String[] args) {
+		 int[] numbers = {10, 20, 30, 40, 50};
+	        int sum = 0;
+
+	        for(int i = 0; i < numbers.length; i++) {
+	            sum = sum + numbers[i];
+	        }
+
+	        System.out.println("Sum = " + sum);
+	    }
+	
+	}
+
+
