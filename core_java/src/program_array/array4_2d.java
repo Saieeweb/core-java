81c8ae0ae1639da041ce4a9d9b75102fa5447258
Saieeweb <saieejagtap70@gmail.com>
2026-09-25 21:48:39 +0530

    array4d

---
+++ core_java/src/program_array/array4_2d.java
@@ -0,0 +1,18 @@
+package program_array;
+
+public class array4_2d {
+
+	public static void main(String[] args) {
+String[][]a= {
+{"a","b","c","d"},
+{"e","f","g","h",},
+{"j","i","h","l"}
+	};
+	for (int i=0; i<4; i++) {
+		for (int j=0; j<4; j++) {
+			System.out.println(a[i][j]);
+		}
+	}
+
+}
+}
